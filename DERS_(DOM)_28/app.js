7734a2df0a28d6928b80df28b3f218d228cec59b
orkunumur <orkunumur@gmail.com>
2023-04-23 14:01:10 +0300

    DOM

--- DERS_(DOM)_28/app.js
+++ DERS_(DOM)_28/app.js
@@ -23,3 +23,28 @@ const sub_title2 = document.querySelector("#sub_title"); // id ile seçme
 const class_name = document.querySelector(".first_div"); // class ile seçme
 console.log(sub_title2)
 console.log(class_name)
+
+//! Liste içerindeki son elemana ulaşmak veya eleman eklemek ve değiştirmek
+let item = document.querySelector("ul#liste>li:first-child").innerHTML // ilk eleman 
+let item1 = document.querySelector("ul#liste>li:last-child").innerHTML // son eleman
+console.log(item)
+console.log(item1)
+
+
+//* Eleman Düzenleme
+let firstChild = document.querySelector("ul#liste>li:first-child")
+firstChild.innerHTML = "İlk Öğe Değişti"
+
+let lastChild = document.querySelector("ul#liste>li:last-child")
+lastChild.innerHTML = "Son Öğe Değişti"
+
+//* Yeni bir eleman ekleme
+let new_element = document.querySelector("ul#liste")
+let new_li = document.createElement("li")
+let new_li1 = document.createElement("li")
+
+new_li.innerHTML = "JavaScript ile eleman eklemek"
+new_li1.innerHTML = "JavaScript ile Başa eleman ekleme"
+
+new_element.append(new_li)   // Sona ekleme
+new_element.prepend(new_li1) // Başa ekleme
