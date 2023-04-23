1287973a997226ebbc4db78886e46d69ab710080
orkunumur <orkunumur@gmail.com>
2023-04-23 21:42:34 +0300

    dom bitti

--- DERS_(DOM)_28/app.js
+++ DERS_(DOM)_28/app.js
@@ -38,7 +38,7 @@ firstChild.innerHTML = "İlk Öğe Değişti"
 let lastChild = document.querySelector("ul#liste>li:last-child")
 lastChild.innerHTML = "Son Öğe Değişti"
 
-//* Yeni bir eleman ekleme
+//* Yeni bir eleman ekleme 1
 let new_element = document.querySelector("ul#liste")
 let new_li = document.createElement("li")
 let new_li1 = document.createElement("li")
@@ -48,3 +48,33 @@ new_li1.innerHTML = "JavaScript ile Başa eleman ekleme"
 
 new_element.append(new_li)   // Sona ekleme
 new_element.prepend(new_li1) // Başa ekleme
+
+//* Yeni bir eleman ekleme 2
+const first_dive = document.querySelector(".first_div");
+const link = document.createElement("a");
+link.id= "goGoogle";
+link.className = "Google-link";
+link.href= "https://www.google.com/search?client=opera&q=google&sourceid=opera&ie=UTF-8&oe=UTF-8";
+link.target="_blank";
+link.innerHTML = "Google";
+
+first_dive.appendChild(link);
+
+//* Eleman Silme
+const eleman_1 = document.querySelector(".list1");
+const liste_1 = document.querySelectorAll(".eleman-1")
+console.log(liste_1)
+// liste_1[0].remove()
+// eleman_1.removeChild(liste_1[0])
+
+//* Elemanları Yer Değiştirme
+const ol_list = document.querySelectorAll(".eleman-1")[0]; //değişecek eleman
+
+let new_li2 = document.createElement("li");
+new_li2.className="new-element"
+
+new_li2.innerHTML="lore aut corrupti atque tempora eius aperiam nesciunt deserunt ex, vero sint odio sed quam eveniet eos!";
+
+ol_list.replaceChild(new_li2, ol_list.childNodes[1])
+console.log(ol_list.childNodes[1])
+
