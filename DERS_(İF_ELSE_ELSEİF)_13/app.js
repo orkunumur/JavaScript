eaa80eb1aa667ccc10699304f5d0ae9d5ff67045
orkunumur <orkunumur@gmail.com>
2023-04-23 22:23:52 +0300

    kodluyoruz eklemeleri

--- DERS_(İF_ELSE_ELSEİF)_13/app.js
+++ DERS_(İF_ELSE_ELSEİF)_13/app.js
@@ -16,5 +16,14 @@ else{
     alert("Lütfen geçerli bir yol seçiniz !!");
 }
 
+// Tenary operatörü
+var money = 40;
+//* Normal tenary
+var buy_sit = money < 10 ? console.log("Can't buy this product"):  console.log("Can buy this product");
 
+//* Zincirleme tenary
+var canBuy = 
+    (money < 17) ? console.log("Satın alamazsın.."):
+    (money > 30) ? console.log("Satın alabilirsin.."):
+    console.log("Para miktarını girmen gerekmektedir..");
 
