a152d300a62d35d37731c21cfeaf455b0f8d6146
orkunumur <orkunumur@gmail.com>
2023-04-28 00:19:16 +0300

    28/04/2023

--- DERS_(EVENTS_I)_28/app.js
+++ DERS_(EVENTS_I)_28/app.js
@@ -1,19 +1,32 @@
 //! ONCLİCK()
-function changeTitle(){
-    document.querySelector("#baslik1").textContent = "EVENTS (OLAYLAR OLAYLAR...)"
+function changeTitle() {
+  TİT1 = document.querySelector("#baslik1");
+  TİT1.textContent == "Events"
+    ? (TİT1.textContent = "EVENTS (OLAYLAR OLAYLAR...)")
+    : (TİT1.textContent = "Events");
 }
 
 //! addEventListener()
 
 //* click
 const press = document.querySelector("#tikla2");
-press.addEventListener("click", function(){
-    alert("Add Event Listener kullandınız")
+let TİT1 = document.querySelector("#baslik1");
+press.addEventListener("click", function () {
+  alert("Add Event Listener kullandınız");
 });
 
 //*load
 window.addEventListener("load", run);
 
-function run(){
-    console.log("Sayfa Yüklendi");
+function run() {
+  console.log("Sayfa Yüklendi");
+}
+
+//* mouseover Events yazısının üstüne git !
+TİT1.addEventListener("mouseover", changeColor);
+
+function changeColor() {
+  this.style.color == "black"
+    ? (this.style.color = "red")
+    : (this.style.color = "black");
 }
